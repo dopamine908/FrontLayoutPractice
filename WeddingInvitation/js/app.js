7975b97c0f41cbbcb353cb3953222582df0e5495
Wu Mobius <energy908@gmail.com>
2020-07-19 10:30:04 +0800

    add wedding invitation - 3rd edition - add first story animation

--- WeddingInvitation/js/app.js
+++ WeddingInvitation/js/app.js
@@ -9,20 +9,36 @@ function showScroll() {
     console.log(scrollVal);
     var first_story_height_distance = $('.story:nth-child(1) *').offset().top - 600;
     if (scrollVal >= first_story_height_distance) {
-        $('.story:nth-child(1) *').addClass('fade-in');
+        console.log('fade in first  story');
+        // $('.story:nth-child(1) *').addClass('fade-in');
+        $('.first-story-lower-photo-fade-out').removeClass('first-story-lower-photo-fade-out');
+        $('.first-story-top-photo-fade-out').removeClass('first-story-top-photo-fade-out');
+        $('.first-story-content-fade-out').removeClass('first-story-content-fade-out');
     }
     var second_story_height_distance = $('.story:nth-child(2) *').offset().top - 600;
     if (scrollVal >= second_story_height_distance) {
-        $('.story:nth-child(2) *').addClass('fade-in');
+        // $('.story:nth-child(2) *').addClass('fade-in');
     }
     var third_story_height_distance = $('.story:nth-child(3) *').offset().top - 600;
 
     if (scrollVal >= third_story_height_distance) {
-        $('.story:nth-child(3) *').addClass('fade-in');
+        // $('.story:nth-child(3) *').addClass('fade-in');
     }
     var fourth_story_height_distance = $('.story:nth-child(4) *').offset().top - 600;
 
     if (scrollVal >= fourth_story_height_distance) {
-        $('.story:nth-child(4) *').addClass('fade-in');
+        // $('.story:nth-child(4) *').addClass('fade-in');
     }
+}
+
+function fadein() {
+    $('.first-story-lower-photo-fade-out').removeClass('first-story-lower-photo-fade-out');
+    $('.first-story-top-photo-fade-out').removeClass('first-story-top-photo-fade-out');
+    $('.first-story-content-fade-out').removeClass('first-story-content-fade-out');
+}
+
+function fadeout() {
+    $('.first-story-lower-layer-photo').addClass('first-story-lower-photo-fade-out');
+    $('.first-story-top-layer-photo').addClass('first-story-top-photo-fade-out');
+    $('.first-story-content').addClass('first-story-content-fade-out');
 }
