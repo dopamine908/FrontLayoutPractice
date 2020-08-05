a1e028c6749be236d4b9df1ab739fba695c085f6
Wu Mobius <energy908@gmail.com>
2020-08-05 23:27:00 +0800

    add baby age - a baby age count static page

---
+++ BabyAge/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    setBabyAge();
+});
+
+
+function setBabyAge() {
+    setMonth();
+    setDay();
+}
+
+function setMonth() {
+    var offsetTime = getOffsetTime();
+    var month = parseInt((((offsetTime / 60) / 60) / 24) / 30);
+    $('#count-month').html(month);
+
+}
+
+function setDay() {
+    var offsetTime = getOffsetTime();
+    var day = parseInt((((offsetTime / 60) / 60) / 24) % 30);
+    $('#count-day').html(day);
+
+}
+
+function getBabyBirthdayDateTime() {
+    return new Date(2020, 1, 19, 12, 0, 0, 0);
+}
+
+function getOffsetTime() {
+    var Now = new Date();
+    var WeddingDateTime = getBabyBirthdayDateTime();
+    // ** 以秒為單位
+    return (Now - WeddingDateTime) / 1000;
+}
